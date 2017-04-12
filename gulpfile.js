cc6d28bb604924b4f3b66f35b632c42b2ad6ae14
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2017-04-12 16:48:36 -0400

    Add automatic building task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,41 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var vinyl = require('vinyl-source-stream');
-var tsify = require('tsify')
+var tsify = require('tsify');
+var watchify = require('watchify');
 
-gulp.task('default', () => {
+const JS_OUTPUT = 'bundle.js';
+const DIST_DIR = 'dist';
+
+function compile() {
   return browserify({
     basedir: '.',
     debug: true,
     entries: ['src/main.ts'],
     cache: {},
     packageCache: {},
-  })
-  .plugin(tsify)
-  .bundle()
-  .pipe(vinyl('bundle.js'))
-  .pipe(gulp.dest('dist'));
-});
+  }).plugin(tsify);
+}
+
+// Compile TypeScript and bundle everything in a single bundle.js file.
+function build() {
+  return compile()
+    .bundle()
+    .pipe(vinyl(JS_OUTPUT))
+    .pipe(gulp.dest(DIST_DIR));
+}
+
+// Recompile TypeScript on file change.
+function watch() {
+  var task = watchify(compile());
+
+  task.on('update', build);
+
+  return task
+    .bundle()
+    .pipe(vinyl(JS_OUTPUT))
+    .pipe(gulp.dest(DIST_DIR));
+}
+
+gulp.task('default', watch);
+gulp.task('build', build);
