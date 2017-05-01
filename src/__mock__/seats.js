0b203e8a9c0ceb40b1d1201d4500befb9204d62b
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2017-05-01 05:44:01 -0400

    remove flow from seats mock

--- src/__mock__/seats.js
+++ src/__mock__/seats.js
@@ -1,11 +1,9 @@
-// @flow
-
 /**
  *  Mock of a list of seats.
  */
 export const seatsData = {
-  '1': { id: 1, x: 50, y: 60, label: 'G-13' },
-  '2': { id: 2, x: 150, y: 60, label: 'G-14' },
-  '3': { id: 3, x: 250, y: 60, label: 'G-15' },
-  '4': { id: 4, x: 350, y: 60, label: 'G-16' },
+  1: { id: 1, x: 50, y: 60, label: 'G-13' },
+  2: { id: 2, x: 150, y: 60, label: 'G-14' },
+  3: { id: 3, x: 250, y: 60, label: 'G-15' },
+  4: { id: 4, x: 350, y: 60, label: 'G-16' },
 };
