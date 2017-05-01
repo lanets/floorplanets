365255733f463aa28e5fa2a755d3cda9d7141583
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2017-04-30 23:05:33 -0400

    Remove unused css and svg

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Header>
+          <h2>Floorplan</h2>
+        </Header>
+      </Wrapper>
     );
   }
 }
