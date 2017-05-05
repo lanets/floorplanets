93a2e807eda9e591a0ef5798ac18421542a1bc86
Jonathan Lalande <lalandej.gg@gmail.com>
2017-05-05 13:04:17 -0400

    Move SeatState type into reducers/types.js
    This is where it belongs since it's only used explicitly inside a reducer.

--- src/reducers/types.js
+++ src/reducers/types.js
@@ -1,6 +1,16 @@
 /* @flow */
-import type { SeatState } from '../types';
 
+
+/*
+ * The data binding between the state of a seat and the graphic
+ * representation of a seat.
+ */
+export type SeatState = {
+  id: number,
+  x: number,
+  y: number,
+  label: string,  // what's written on the seat
+};
 
 /**
  *  Hashmap of seats.
