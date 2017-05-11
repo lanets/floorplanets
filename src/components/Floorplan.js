a33b8bfe174c1bc3267640953f9de5233ed42765
Jonathan Lalande <lalandej.gg@gmail.com>
2017-05-11 13:23:26 -0400

    Add occlusion optimization to the seats rendering.

--- src/components/Floorplan.js
+++ src/components/Floorplan.js
@@ -16,6 +16,14 @@ export default class Floorplan extends React.Component {
   props: Props;
   view: paper.view;
 
+  seats: Seat[];
+  update: () => void;
+
+  constructor(props: Props) {
+    super(props);
+    this.update = this.update.bind(this);
+  }
+
   componentDidMount() {
     const canvas = this.refs.canvas;
 
@@ -26,19 +34,22 @@ export default class Floorplan extends React.Component {
 
     // view bindings
     this.view = paper.view;
+    this.view.autoUpdate = false;
     this.view.onMouseDrag = (e) => this.translateCamera(e);
 
+    // HARDCODED: center the view for our mocked seats
     this.view.center = new Point(1175, 371);
 
     // Render the seats based on the loaded data.
-    const seats: Seat[] = [];
+    this.seats = [];
     for (const id in this.props.seats) {
       const seatdata = this.props.seats[id];
       const seat = new Seat(seatdata.x, seatdata.y, seatdata.label);
-      seats.push(seat);
+      this.seats.push(seat);
     }
 
     console.log('Floorplan initialized.');
+    this.update();
   }
 
   translateCamera(event: Object) {
@@ -51,7 +62,21 @@ export default class Floorplan extends React.Component {
     this.view.center = this.view.center.add(new Point(-delta.x, -delta.y));
 
     event.preventDefault()
-    this.view.draw();
+  }
+
+  update() {
+    // hide seats that are not in the view
+    for (let i = 0; i < this.seats.length; i++)
+      this.applyOcclusion(this.seats[i]);
+
+    this.view.update();
+
+    requestAnimationFrame(this.update);
+  }
+
+  applyOcclusion(seat: Seat) {
+    // hide seats that are not in the bounds of the view.
+    seat.visible = seat.position.isInside(this.view.bounds);
   }
 
   render() {
--- src/components/Seat.js
+++ src/components/Seat.js
@@ -11,42 +11,27 @@ export default class Seat {
   shape: Shape;
   text: PointText;
 
+  visible: boolean;
+
   constructor(x: number, y: number, label: string) {
     this.position = new Point(x, y);
 
     const radius = 9;
-    const outCircle = Shape.Circle({
+    this.item = Shape.Circle({
       position: this.position,
       radius,
       fillColor: FLAT_COLORS.POMEGRANATE,
       strokeColor: 'black',
     });
 
-    const inCircle = Shape.Circle({
-      position: this.position,
-      radius: radius * 0.8,
-      fillColor: FLAT_COLORS.ALIZARIN,
-    });
-
-    // this.text = new PointText({
-      // position: new Point(this.position.x, this.position.y + 6 * 0.5),
-      // content: label,
-      // fillColor: 'black',
-      // fontFamily: 'Helvetica',
-      // fontSize: 6,
-      // justification: 'center',
-    // });
-
-
-    this.item = new Group({
-      position: this.position,
-      children: [
-        outCircle,
-        inCircle,
-        // this.text,
-      ],
-    });
-
     this.item.onClick = () => console.log(this.item);
+  }
+
+  get visible(): boolean {
+    return this.item.visible;
+  }
+
+  set visible(val: boolean) {
+    this.item.visible = val;
   }
 }
