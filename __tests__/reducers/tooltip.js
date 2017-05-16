4b6153acf5747bbdd604ff2474b1a5028b14ed0a
Jonathan Lalande <lalandej.gg@gmail.com>
2017-05-16 08:54:56 -0400

    add a test to make sure empty state fallback to default state.

--- __tests__/reducers/tooltip.js
+++ __tests__/reducers/tooltip.js
@@ -2,6 +2,16 @@ import reducer, { initial } from '../../src/reducers/tooltip';
 import { showTooltip } from '../../src/actions/tooltip';
 
 
+describe('Passing an empty state to the tooltip reducer', () => {
+  it('uses a default `initial` state', () => {
+    const action = showTooltip('some text goes here');
+    const before = undefined;
+    const after = { ...before, display: true };
+
+    expect(reducer(before, action)).toEqual(after);
+  });
+});
+
 describe('reducer handles SHOW_TOOLTIP action', () => {
   it('sets the display boolean to true', () => {
     const action = showTooltip('some text goes here');
