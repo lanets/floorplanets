a07c577e6f8f77ceadf35529a623f9f07adde6e1
Jonathan Lalande <lalandej.gg@gmail.com>
2017-05-16 12:18:05 -0400

    add tooltip reducer to combined reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,10 @@ import { combineReducers } from 'redux';
 
 import seats from './seats';
 import zoom from './zoom';
+import tooltip from './tooltip';
 
 export default combineReducers({
   seats,
   zoom,
+  tooltip,
 });
