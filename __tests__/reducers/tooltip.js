dcfba5aff933c24079df2a22698fbd05c81c5fcc
Jonathan Lalande <lalandej.gg@gmail.com>
2017-05-16 09:04:38 -0400

    remove default state test case.
    It's irrelevant.

--- __tests__/reducers/tooltip.js
+++ __tests__/reducers/tooltip.js
@@ -2,22 +2,21 @@ import reducer, { initial } from '../../src/reducers/tooltip';
 import { showTooltip } from '../../src/actions/tooltip';
 
 
-describe('Passing an empty state to the tooltip reducer', () => {
-  it('uses a default `initial` state', () => {
-    const action = showTooltip('some text goes here');
-    const before = undefined;
-    const after = { ...before, display: true };
-
-    expect(reducer(before, action)).toEqual(after);
-  });
-});
-
 describe('reducer handles SHOW_TOOLTIP action', () => {
   it('sets the display boolean to true', () => {
     const action = showTooltip(50, 100, 'some text goes here');
     const before = initial;
-    const after = { ...before, display: true, x: 50, y: 100 };
+    const after = reducer(before, action);
 
-    expect(reducer(before, action)).toEqual(after);
+    expect(after.display).toEqual(true);
+  });
+
+  it('sets the x and y values to the action values', () => {
+    const action = showTooltip(50, 100, 'some text goes here');
+    const before = initial;
+    const after = reducer(before, action);
+
+    expect(after.x).toEqual(50);
+    expect(after.y).toEqual(100);
   });
 });
