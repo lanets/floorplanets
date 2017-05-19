f2a21d334f6c661399118703481da491427aa5dc
aviau <alexandre@alexandreviau.net>
2017-05-18 21:02:16 -0400

    floorplanets: allow setting port

--- Makefile
+++ Makefile
@@ -52,7 +52,7 @@ nodetest-CI: node_modules .node-build-image eslint flow
 
 .PHONY: gobuild
 gobuild: .golang-build-image
-	$(docker_run_go) bash -c "go get -v ./... && go build ./... && go build ./.../cmd/..."
+	$(docker_run_go) bash -c "go get -v ./... && go build ./... && go build github.com/lanets/floorplanets/backend/cmd/floorplanets"
 
 .PHONY: gofmt
 gofmt: .golang-build-image
---
+++ backend/cmd/floorplanets/cli/cli_test.go
@@ -0,0 +1,23 @@
+package cli_test
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/lanets/floorplanets/backend/cmd/floorplanets/cli"
+)
+
+func TestHelp(t *testing.T) {
+	var writer bytes.Buffer
+
+	cli.Run([]string{"--help"}, &writer)
+	assert.True(t, strings.Contains(writer.String(), "Usage"))
+
+	writer.Reset()
+
+	cli.Run([]string{"-h"}, &writer)
+	assert.True(t, strings.Contains(writer.String(), "Usage"))
+}
